6714d316f43d0a16f2c03c3acce1b2143f840dec
Alexis Leite <alexisleite.uy@gmail.com>
2026-03-21 14:25:06 -0300

    trivial api handler for test

--- apps/api/api/[...all].ts
+++ apps/api/api/[...all].ts
@@ -1,19 +1,9 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import serverless from "serverless-http";
-import { createVercelApp } from "../src/bootstrap";
 
-let cachedHandler: ReturnType<typeof serverless> | null = null;
-
-async function getHandler() {
-  if (!cachedHandler) {
-    const app = await createVercelApp();
-    cachedHandler = serverless(app);
-  }
-
-  return cachedHandler;
-}
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const appHandler = await getHandler();
-  return appHandler(req, res);
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.status(200).json({
+    ok: true,
+    method: req.method,
+    url: req.url
+  });
 }
